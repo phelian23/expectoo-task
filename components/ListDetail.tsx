a41add744084239a1c1b707fabf7e7d802650d41
Oluwafemi Awoyemi <femikola23@gmail.com>
2023-03-03 22:45:10 +0100

    Add list detail component

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react'
 
-import { User } from '../interfaces'
+import { Character } from '../interfaces'
 
 type ListDetailProps = {
-  item: User
+  item: Character
 }
 
-const ListDetail = ({ item: user }: ListDetailProps) => (
+const ListDetail = ({ item: character }: ListDetailProps) => (
   <div>
-    <h1>Detail for {user.name}</h1>
-    <p>ID: {user.id}</p>
+    <h1>Detail for {character.name}</h1>
+    <img src={character.image} alt={character.name} />
+    <ul>
+      <li>
+        <strong>Species:</strong> {character.species}
+      </li>
+      <li>
+        <strong>Status:</strong> {character.status}
+      </li>
+      <li>
+        <strong>Created at:</strong> {character.created}
+      </li>
+      <li>
+        <strong>Gender:</strong> {character.gender}
+      </li>
+      <li>
+        <strong>Location:</strong> {character.location.name}
+      </li>
+      <li>
+        <strong>Episodes:</strong> {character.episode.length}
+      </li>
+    </ul>
   </div>
 )
 
