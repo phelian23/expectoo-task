bcbdabe32e5cfd171f001a200faf546f0c4128c4
Oluwafemi Awoyemi <femikola23@gmail.com>
2023-03-03 23:32:56 +0100

    Add date handler

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -6,31 +6,50 @@ type ListDetailProps = {
   item: Character
 }
 
-const ListDetail = ({ item: character }: ListDetailProps) => (
-  <div>
-    <h1>Detail for {character.name}</h1>
-    <img src={character.image} alt={character.name} />
-    <ul>
-      <li>
-        <strong>Species:</strong> {character.species}
-      </li>
-      <li>
-        <strong>Status:</strong> {character.status}
-      </li>
-      <li>
-        <strong>Created at:</strong> {character.created}
-      </li>
-      <li>
-        <strong>Gender:</strong> {character.gender}
-      </li>
-      <li>
-        <strong>Location:</strong> {character.location.name}
-      </li>
-      <li>
-        <strong>Episodes:</strong> {character.episode.length}
-      </li>
-    </ul>
-  </div>
-)
+const ListDetail = ({ item: character }: ListDetailProps) => {
+  const getDay = (date) => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[new Date(date).getDay()];
+  }
+
+  const getMonth = (date) => {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return months[new Date(date).getMonth()];
+  }
+
+  const dateHandler = (date) => {
+    const dateObj = new Date(date);
+    const day = dateObj.getDate();
+    const year = dateObj.getFullYear();
+    return `${getDay(date)} ${day} ${getMonth(date)} ${year}`;
+  }
+
+  return (
+    <div>
+      <h1>Detail for {character.name}</h1>
+      <img src={character.image} alt={character.name} />
+      <ul>
+        <li>
+          <strong>Species:</strong> {character.species}
+        </li>
+        <li>
+          <strong>Status:</strong> {character.status}
+        </li>
+        <li>
+          <strong>Created at:</strong> {dateHandler(character.created)}
+        </li>
+        <li>
+          <strong>Gender:</strong> {character.gender}
+        </li>
+        <li>
+          <strong>Location:</strong> {character.location.name}
+        </li>
+        <li>
+          <strong>Episodes:</strong> {character.episode.length}
+        </li>
+      </ul>
+    </div>
+  )
+}
 
 export default ListDetail
